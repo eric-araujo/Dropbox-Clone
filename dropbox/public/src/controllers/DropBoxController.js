0581938f6967191ad2c03685a88135efc990e8d8
eric-araujo <eric.araujolima@hotmail.com>
2020-07-04 19:03:48 -0300

    Upload de arquivo com formidable

--- dropbox/public/src/controllers/DropBoxController.js
+++ dropbox/public/src/controllers/DropBoxController.js
@@ -1,13 +1,13 @@
 class DropBoxController {
 
     constructor() {
-    
+
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFileEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
 
         this.initEvent();
-    
+
     }
 
     initEvent() {
@@ -15,16 +15,62 @@ class DropBoxController {
         this.btnSendFileEl.addEventListener('click', event => {
 
             this.inputFileEl.click();
-        
+
         });
 
         this.inputFileEl.addEventListener('change', event => {
 
-            console.log(event.target.files);
+            this.uploadTask(event.target.files);
 
             this.snackModalEl.style.display = 'block';
 
         });
+
+    }
+
+    uploadTask(files) {
+
+        let promises = [];
+
+        [...files].forEach(file => {
+
+            promises.push(new Promise((resolve, rejetct) => {
+
+                const ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+
+                    try {
+
+                        resolve(JSON.parse(ajax.responseText));
+
+                    } catch (e) {
+
+                        rejetct(e)
+
+                    }
+
+                }
+
+                ajax.onerror = event => {
+
+                    rejetct(event);
+
+                }
+
+                const formData = new FormData();
+
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+
+        });
+
+        return Promise.all(promises);
 
     }
 
